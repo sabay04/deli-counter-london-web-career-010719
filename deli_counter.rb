e2aa18f0ae351a9eb832ba32d546ca8efdea4f16
github username <github email address>
2018-12-15 09:28:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,43 @@
 # Write your code here.
+
+
+def line(katz_deli)
+ if katz_deli.size == 0
+     puts "The line is currently empty."
+ else
+    line = "The line is currently:"
+    
+     katz_deli.each_with_index do |name , num |
+         
+         line += " #{num+1}. #{name}"
+         
+        end
+     puts "#{line}"
+     end
+    
+end
+
+
+
+
+def take_a_number(array,name)
+
+  array.push(name)
+  number = array.index(name)
+  puts "Welcome, #{name}. You are number #{number+1} in line."
+  return name, number
+  
+end
+
+
+
+def now_serving(array)
+    
+    if array.size == 0
+        puts "There is nobody waiting to be served!"
+    
+    else
+    
+    puts "Currently serving #{array.shift}."
+    end
+end
